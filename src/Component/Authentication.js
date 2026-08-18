e4cf7524f82ab17bb9ed318a24b59d22266aba00
Tejash Kumar <tejashkumar1316@gmail.com>
2026-08-18 20:05:07 +0530

    fixed login page error message

--- src/Component/Authentication.js
+++ src/Component/Authentication.js
@@ -33,10 +33,6 @@ export default function Authentication({ setUsername }) {
 
       const data = await response.json();
 
-      if (!response.ok) {
-        throw new Error(data.message || "Something went wrong");
-      }
-
       if (data.message) {
         console.log("true");
         setMessage("");
